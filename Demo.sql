596d3483eea5667aa9df8760247f0b34b411be5a
Paulayan009 <49440768+Paulayan009@users.noreply.github.com>
2019-04-12 11:04:18 +0530

    Demo Added

--- Demo.sql
+++ Demo.sql
@@ -1,4 +1,4 @@
 -- Select rows from a Table or View 'TableOrViewName' in schema 'SchemaName'
 SELECT * FROM Dbo.Employee
-WHERE X=Z	/* add search conditions here */
+WHERE X=Y	/* add search conditions here */
 GO
